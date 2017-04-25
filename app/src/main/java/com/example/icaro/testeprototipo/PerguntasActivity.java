e66f9ef5a88d7c84a03da78d991c42975045ba2b
Thiago <thiago_483@yahoo.com.br>
2017-04-25 19:43:07 -0300

    Thiago: Teste

---
+++ app/src/main/java/com/example/icaro/testeprototipo/PerguntasActivity.java
@@ -0,0 +1,13 @@
+package com.example.icaro.testeprototipo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class PerguntasActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_perguntas);
+    }
+}
